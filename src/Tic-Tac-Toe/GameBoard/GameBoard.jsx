1f1c24c6b235b19734c94fbc65faa0547d158700
Abay <ibrayev.abay@gmail.com>
2023-11-14 20:48:04 -0700

    items

--- src/Tic-Tac-Toe/GameBoard/GameBoard.jsx
+++ src/Tic-Tac-Toe/GameBoard/GameBoard.jsx
@@ -3,9 +3,16 @@ import { GridItem } from "../GridItem/GridItem";
 
 export default function OrderHistoryPage() {
     // 0 - empty | 1 - first player | 2 - second player
+    const [currentPlayer, setCurrentPlayer] = useState(1)
     const [boardState, setBoardState] = useState([0,0,0,0,0,0,0,0,0])
+    
+    function handleClick() {
+        const player = currentPlayer === 1 ? 2 : 1;
+        setCurrentPlayer(player)
+    }
+
     const GridItems = boardState.map((item, idx) => 
-        <GridItem key={idx} gridItemLocation={idx} gridItemState={item} setGridItemState={setBoardState}/>
+        <GridItem click={handleClick} key={idx} gridItemLocation={idx} gridItemState={item} setGridItemState={setBoardState}/>
     )
     return (
         <main>
--- src/Tic-Tac-Toe/GridItem/GridItem.jsx
+++ src/Tic-Tac-Toe/GridItem/GridItem.jsx
@@ -1,17 +1,20 @@
 import { useState, useRef, useEffect } from "react"
 
-export function GridItem({ gridItemLocation, gridItemState, setGridItemState }) {
+export function GridItem({ gridItemLocation, gridItemState, setGridItemState, click }) {
     const [itemState, setItemState] = useState(gridItemState)
     const player = useRef(null)
+
     useEffect(function () {
         player.current = "None"
-        if (itemState !== 0) player.current = player.current===1 ? "X" : "O"
+        if (itemState !== 0) player.current = player.current===1 ? "X" : "O";
     }, [itemState])
+
     return (
         <div>
             <h1>{player.current}</h1>
             <h2>{gridItemLocation}</h2>
             <h2>{gridItemState}</h2>
+            <button onClick={click}>Click here</button>
             {/* {some Function} */}
         </div>
     )
