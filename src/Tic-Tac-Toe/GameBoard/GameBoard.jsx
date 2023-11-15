2242286597b35317daf0fa19fd11f649380d118c
Abay <ibrayev.abay@gmail.com>
2023-11-14 21:21:28 -0700

    clicking

--- src/Tic-Tac-Toe/GameBoard/GameBoard.jsx
+++ src/Tic-Tac-Toe/GameBoard/GameBoard.jsx
@@ -5,15 +5,10 @@ export default function OrderHistoryPage() {
     // 0 - empty | 1 - first player | 2 - second player
     const [currentPlayer, setCurrentPlayer] = useState(1)
     const [boardState, setBoardState] = useState([0,0,0,0,0,0,0,0,0])
-    
-    function handleClick() {
-        const player = currentPlayer === 1 ? 2 : 1;
-        setCurrentPlayer(player)
-    }
-
     const GridItems = boardState.map((item, idx) => 
-        <GridItem click={handleClick} key={idx} gridItemLocation={idx} gridItemState={item} setGridItemState={setBoardState}/>
+    <GridItem setCurrentPlayer={setCurrentPlayer} currentPlayer={currentPlayer} boardState={boardState} key={idx} gridItemLocation={idx} gridItemState={item} setGridItemState={setBoardState}/>
     )
+    
     return (
         <main>
             <h1>Test</h1>
--- src/Tic-Tac-Toe/GridItem/GridItem.jsx
+++ src/Tic-Tac-Toe/GridItem/GridItem.jsx
@@ -1,20 +1,26 @@
 import { useState, useRef, useEffect } from "react"
 
-export function GridItem({ gridItemLocation, gridItemState, setGridItemState, click }) {
-    const [itemState, setItemState] = useState(gridItemState)
-    const player = useRef(null)
+export function GridItem({ gridItemLocation, gridItemState, setGridItemState, setCurrentPlayer, currentPlayer, boardState }) {
+    const player = useRef(currentPlayer);
 
-    useEffect(function () {
-        player.current = "None"
-        if (itemState !== 0) player.current = player.current===1 ? "X" : "O";
-    }, [itemState])
+    const newBoardState = boardState;
+    
+    function handleClick() {
+        if (gridItemState === 0) {
+            newBoardState[gridItemLocation] = player.current;
+            setGridItemState(newBoardState);
+            player.current = currentPlayer === 1 ? 2 : 1;
+            setCurrentPlayer(player.current)
+            console.log('the current player: ', player.current)
+        }
+    }
 
+    
     return (
         <div>
-            <h1>{player.current}</h1>
             <h2>{gridItemLocation}</h2>
             <h2>{gridItemState}</h2>
-            <button onClick={click}>Click here</button>
+            <button onClick={handleClick}>Click here</button>
             {/* {some Function} */}
         </div>
     )
