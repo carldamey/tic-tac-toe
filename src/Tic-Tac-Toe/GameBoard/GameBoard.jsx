d963e73db00a102170dc151464a54b1f79df3fe1
Abay <ibrayev.abay@gmail.com>
2023-11-14 20:23:27 -0700

    inital board

--- src/Tic-Tac-Toe/GameBoard/GameBoard.jsx
+++ src/Tic-Tac-Toe/GameBoard/GameBoard.jsx
@@ -1,18 +1,17 @@
+import { useState, useEffect, useRef } from 'react';
 import { GridItem } from "../GridItem/GridItem";
 
 export default function OrderHistoryPage() {
+    // 0 - empty | 1 - first player | 2 - second player
+    const [boardState, setBoardState] = useState([0,0,0,0,0,0,0,0])
 
+    const GridItems = boardState.forEach(item => {
+        <GridItem gridItemLocation={boardState.indexOf(item)} gridItemState={item} setGridItemState={setBoardState}/>
+    })
+    
     return (
         <main>
-            <GridItem />
-            <GridItem />
-            <GridItem />
-            <GridItem />
-            <GridItem />
-            <GridItem />
-            <GridItem />
-            <GridItem />
-            <GridItem />
+            { GridItems }
         </main>
     )
 }
