515f2f68499386a2c7381d7c2b611cf0b529224c
Abay <ibrayev.abay@gmail.com>
2023-11-14 19:52:23 -0700

    framework

---
+++ src/Tic-Tac-Toe/GameBoard/GameBoard.jsx
@@ -0,0 +1,18 @@
+import { GridItem } from "../GridItem/GridItem";
+
+export default function OrderHistoryPage() {
+
+    return (
+        <main>
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem />
+        </main>
+    )
+}
