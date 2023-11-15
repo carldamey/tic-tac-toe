518f7c4154a3d305ee56acc244f10d238ad82f80
Abay <ibrayev.abay@gmail.com>
2023-11-15 15:10:10 -0700

    button color 1/2

--- src/Tic-Tac-Toe/GridItem/GridItem.jsx
+++ src/Tic-Tac-Toe/GridItem/GridItem.jsx
@@ -2,22 +2,23 @@
 import { useState, useRef, useEffect } from "react"
 import "./GridItem.css"
 
-export function GridItem({ gridItemLocation, gridItemState, setGridItemState, setCurrentPlayer, currentPlayer, boardState }) {
+export function GridItem({ gridItemLocation, gridItemState, setGridItemState, setCurrentPlayer, currentPlayer, boardState, winner }) {
     const player = useRef();
-
+    const buttonColour = useRef(null);
     const [isActive, setActive] = useState("false");
 
 
     function handleClick() {
-        if (gridItemState === 0) {
+        if (gridItemState === 0 && !winner) {
             const newBoardState = boardState;
             player.current = currentPlayer;
             console.log('current player: ', player.current, ' and item location is: ', gridItemLocation)
             newBoardState[gridItemLocation] = player.current;
+            buttonColour.current = player.current;
             player.current = currentPlayer !== 1 ? 1 : 2;
             console.log('the new board state: ', newBoardState)
             setGridItemState(newBoardState);
-            setCurrentPlayer(player.current)
+            setCurrentPlayer(player.current);
             setActive(!isActive);
         }
     }
@@ -30,7 +31,7 @@ export function GridItem({ gridItemLocation, gridItemState, setGridItemState, se
     
     return (
         <div className="gridItem">
-            <button onClick={handleClick} className={`button-${isActive ? "not-active" : "active"}`}>{icon}</button>
+            <button onClick={handleClick} className={`button-${isActive ? "not-active" : "active"} ${buttonColour.current}`}>{icon}</button>
         </div>
     )
 }
